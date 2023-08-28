6d3fa1af8d82e479a905fee8339e2378e6618296
vcvaraman <vcvaram@gmail.com>
2023-08-28 19:28:31 +0530

    Update SpeakingclockApplicationTest.java

--- src/test/java/com/task/speakingclock/SpeakingclockApplicationTest.java
+++ src/test/java/com/task/speakingclock/SpeakingclockApplicationTest.java
@@ -1,1 +1,38 @@
+package com.task.speakingclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpeakingclockApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testConvertToWords_Success() {
+        String url = "http://localhost:" + port + "/convert?time=08:34";
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("It's eight thirty-four", response.getBody());
+    }
+
+    @Test
+    public void testConvertToWords_InvalidFormat() {
+        String url = "http://localhost:" + port + "/convert?time=08:34:59";
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Invalid time format", response.getBody());
+    }
+}
 
