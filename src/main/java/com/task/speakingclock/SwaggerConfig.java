a66cc749402ad3a0216ce9564b62dd3db3ce54c6
vcvaraman <vcvaram@gmail.com>
2023-08-28 21:01:57 +0530

    Update SwaggerConfig.java

--- src/main/java/com/task/speakingclock/SwaggerConfig.java
+++ src/main/java/com/task/speakingclock/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.speakingclock.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.task.speakingclock.controller"))
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
